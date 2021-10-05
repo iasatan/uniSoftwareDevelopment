2d94d05282d7192d347138555c87fdfa14d01fa7
mrbalazs5 <megbalaa@gmail.com>
2021-10-05 17:17:15 +0200

    #4 SQL queries

--- sql/music_app.sql
+++ sql/music_app.sql
@@ -24,3 +24,82 @@ mid int(11) NOT NULL auto_increment primary key,
 );
 
 create table user_music(uid int, foreign key(uid) references user(uid), mid int, foreign key(mid) references music(mid), rating float, lastPlayed datetime, numberOfPlay int);
+
+insert into user(name, email, password, gender, address, typeOfResidence) values ("Megyeri Balázs", "mrbalazs@gmail.com", "12345678", "male", "Miskolc", "flat")
+insert into genre(gid, name) values (2, "jazz"), (3, "funk"), (4, "electronic")
+
+insert into music(
+    filename,
+    artist,
+    title,
+    album,
+    comment,
+    track,
+    year,
+    genre,
+    path,
+    filesize,
+    copyright,
+    layer,
+    stereo,
+    goodframes,
+    badframes,
+    frequency,
+    playtime
+) values (
+    "spanom.mp3", 
+    "Dzsúdló", 
+    "SPANOM", 
+    "Fotofóbia", 
+    "Comment", 
+    10, 
+    2020, 
+    "pop", 
+    "/music/spanom.mp3", 
+    255, 
+    "copyright1", 
+    "layer1", 
+    "stereo1", 
+    100, 
+    0, 
+    30, 
+    211
+  ),
+  (
+    "mindenki_tancol.mp3", 
+    "Majka", 
+    "Mindenki táncol", 
+    "Mindenki táncol", 
+    "Comment2", 
+    1, 
+    2017, 
+    "pop", 
+    "/music/mindenki_tancol.mp3", 
+    255, 
+    "copyright2", 
+    "layer2", 
+    "stereo2", 
+    100, 
+    5, 
+    100, 
+    328
+  ),
+   (
+    "requiem.mp3", 
+    "W.A. Mozart", 
+    "Requiem in D minor", 
+    "Requiem in D minor", 
+    "Comment3", 
+    1, 
+    1791, 
+    "classic", 
+    "/music/requiem.mp3", 
+    255, 
+    "copyright3", 
+    "layer3", 
+    "stereo3", 
+    100, 
+    0, 
+    500, 
+    533
+  )
