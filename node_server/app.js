1a6ea2cbefc49aeecd7dbd31f8d8c96c50f50f56
mrbalazs5 <megbalaa@gmail.com>
2021-11-09 12:05:59 +0100

    #19 List music

--- node_server/app.js
+++ node_server/app.js
@@ -5,30 +5,17 @@ const port = process.env.PORT || 8080;
 
 const db = require('./api/db/db');
 const user = require('./api/user');
+const music = require('./api/music');
 
 const app = express();
 
 app.use(express.json());
+
 app.use(user);
+app.use(music);
+
 app.listen(port, () => {
     console.log('Server is running on ' + port + ".");
 });
-
-app.get('/users', (req, res) => {
-    let getUsers = 'select * from user';
-    db.query(getUsers, (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.status(400).send();
-        }
-
-        if (result == null) {
-            console.log(result);
-            return res.status(400).send();
-        }
-
-        return res.send(result);
-    });
-})
 
 app.use(express.static(path.join(__dirname, 'public')));
